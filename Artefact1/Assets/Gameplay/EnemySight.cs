b7ce4f1ff77be85718a1f8fd47c663dafae2660e
Solomon Sylvester <118912234+SKSylvester@users.noreply.github.com>
2023-11-29 19:13:47 +0000

    Enemy Rotation

--- Artefact1/Assets/Gameplay/EnemySight.cs
+++ Artefact1/Assets/Gameplay/EnemySight.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class EnemySight : MonoBehaviour
 {
+    private Rigidbody2D Enemyrb;
+    private float rotationSpeed = 25f; // Rotate the object around the z-axis 25 degress per second
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Enemyrb = GetComponent<Rigidbody2D>();// Get the enemy RigidBody 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.rotation *= Quaternion.Euler(0f, 0f, rotationSpeed * Time.deltaTime); //  Will rotate the object around at the rate of the rotationSpeed
     }
 }
